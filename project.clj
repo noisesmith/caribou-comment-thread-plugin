859d9e74dc75451577811df1066092a5b5d39bde
Justin Smith <noisesmith@gmail.com>
2014-04-07 10:33:41 -0700

    version 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject caribou/comment-plugin "0.0.1"
+(defproject caribou/comment-plugin "0.0.2"
   :description "A plugin to add threaded comments with an ajax api to a ring site."
   :dependencies [[caribou/caribou-plugin "0.2.0"]]
   :license {:name "Eclipse Public License - v 1.0"
